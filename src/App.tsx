866989739bf20987bafde32d8822021734161fc8
nahidn228 <nahidn228@gmail.com>
2025-06-27 10:20:50 +0600

    project setup done with shadcn and ts

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { Button } from "./components/ui/button";
+
+function App() {
+  return (
+    <>
+      <h1>Basic Todo With Redux</h1>
+      <Button>Test</Button>
+    </>
+  );
+}
+
+export default App;
