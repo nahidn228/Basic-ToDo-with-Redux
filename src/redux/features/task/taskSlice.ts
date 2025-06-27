51d3b8d9d8731a32022036726d1e0f6928339854
nahidn228 <nahidn228@gmail.com>
2025-06-27 22:44:49 +0600

    initiate todo slice and initial state and create types for task and initialState

--- src/redux/features/task/taskSlice.ts
+++ src/redux/features/task/taskSlice.ts
@@ -1,14 +1,29 @@
+import type { ITask } from "@/types";
 import { createSlice } from "@reduxjs/toolkit";
 
-const initialState = {
-  tasks: {
-    id: "asdghhklkl",
-    title: "initialize frontend",
-    description: "create home page and routing",
-    dueDate: "2025-11",
-    isCompleted: false,
-    priority: "high",
-  },
+interface IInitialState {
+  task: ITask[];
+}
+
+const initialState: IInitialState = {
+  task: [
+    {
+      id: "asdghhklkl",
+      title: "initialize frontend",
+      description: "create home page and routing",
+      dueDate: "2025-11",
+      isCompleted: false,
+      priority: "high",
+    },
+    {
+      id: "asdghhklkldfcd",
+      title: "Connect to github",
+      description: "create home page and routing",
+      dueDate: "2025-11",
+      isCompleted: false,
+      priority: "high",
+    },
+  ],
 };
 
 const taskSlice = createSlice({
